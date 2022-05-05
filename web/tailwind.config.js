89e4824375023cb71605f3be80e3ba9babac4f7c
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-05-05 23:38:18 +0100

    added nextjs with tailwind frontend project

---
+++ web/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1400px'
+    },
+    extend: {}
+  },
+  plugins: [],
+}
