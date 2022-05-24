e73c89817eb669fa1796748e03ab53d27cbd4edd
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-05-24 01:09:23 +0100

    added react-table display for transactions

--- web/src/pages/account/[id].tsx
+++ web/src/pages/account/[id].tsx
@@ -1,14 +1,16 @@
 import { withUrqlClient } from 'next-urql'
 import { useRouter } from 'next/router'
+import React from 'react'
 import { Layout } from '../../components/Layout'
 import { useAccountQuery, useTransactionsQuery } from '../../generated/graphql'
 import { createUrqlClient } from '../../utils/createUrqlClient'
+import { useTable } from 'react-table'
 
 const Account = () => {
   const router = useRouter()
   const intId =
     typeof router.query.id === 'string' ? parseInt(router.query.id) : -1
-  const [{ data }] = useAccountQuery({
+  const [account] = useAccountQuery({
     pause: intId === -1,
     variables: { id: intId },
   })
@@ -18,19 +20,85 @@ const Account = () => {
     variables: { accountId: intId },
   })
 
+  const data = React.useMemo(
+    () =>
+      transactions.data?.transactions.map((t) => ({ ...t, accountId: intId })),
+    [transactions.data, intId]
+  )
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Date',
+        accessor: 'date',
+      },
+      {
+        Header: 'Note',
+        accessor: 'note',
+      },
+      {
+        Header: 'Amount',
+        accessor: 'amount',
+      },
+      {
+        Header: 'Type',
+        accessor: 'type',
+      },
+    ],
+    []
+  ) as any
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: data || [] })
+
   return (
     <Layout>
       <div>
-        <h1>{data?.account?.name}</h1>
-        <p>Balance: £{data?.account?.balance}</p>
-        <h2>Transactions:</h2>
-        <ul>
-          {transactions.data?.transactions.map((transaction) => (
-            <li key={transaction.id}>
-              {transaction.note} - £{transaction.amount}
-            </li>
-          ))}
-        </ul>
+        <h1>{account.data?.account?.name}</h1>
+        <p>Balance: £{account.data?.account?.balance}</p>
+      </div>
+      <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+        <table
+          {...getTableProps()}
+          className="min-w-full divide-y divide-gray-200"
+        >
+          <thead className="bg-gray-50">
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                    {...column.getHeaderProps()}
+                  >
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody
+            className="divide-y divide-gray-200 bg-white"
+            {...getTableBodyProps()}
+          >
+            {rows.map((row) => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td
+                        className="whitespace-nowrap px-6 py-4"
+                        {...cell.getCellProps()}
+                      >
+                        {cell.render('Cell')}
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     </Layout>
   )
